137fc9c51ea3bfdd09eb2ffc2c64b2031a729045
Gayatri Srinivasan <gayatrivasan1@gmail.com>
2024-07-30 23:27:34 +0530

    Updated backend URL to https

--- FrontEnd/src/app/demo/services/api.service.ts
+++ FrontEnd/src/app/demo/services/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-//http://127.0.0.1:5000
+//https://peerconnecthubflask.onrender.com
 @Injectable({
   providedIn: 'root'
 })
@@ -9,30 +9,30 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   register(data): Observable<any> {
-    return this.http.post<any>('http://127.0.0.1:5000/register', data, { withCredentials: true });
+    return this.http.post<any>('https://peerconnecthubflask.onrender.com/register', data, { withCredentials: true });
   }
 
   login(data): Observable<any> {
-    return this.http.post('http://127.0.0.1:5000/login', data, { withCredentials: true });
+    return this.http.post('https://peerconnecthubflask.onrender.com/login', data, { withCredentials: true });
   }
 
   logout(): Observable<any> {
-    return this.http.post('http://127.0.0.1:5000/logout', {}, { withCredentials: true });
+    return this.http.post('https://peerconnecthubflask.onrender.com/logout', {}, { withCredentials: true });
   }
 
   findMyMentors(): Observable<any> {
-    return this.http.get<any>('http://127.0.0.1:5000/findMyMentors', { withCredentials: true });
+    return this.http.get<any>('https://peerconnecthubflask.onrender.com/findMyMentors', { withCredentials: true });
   }
 
   isLoggedIn(): Observable<any> {
-    return this.http.get<any>('http://127.0.0.1:5000/isLoggedIn', { withCredentials: true });
+    return this.http.get<any>('https://peerconnecthubflask.onrender.com/isLoggedIn', { withCredentials: true });
   }
 
   getUserProfile(): Observable<any> {
-    return this.http.get<any>('http://127.0.0.1:5000/getUserProfile', { withCredentials: true });
+    return this.http.get<any>('https://peerconnecthubflask.onrender.com/getUserProfile', { withCredentials: true });
   }
 
   updateUserProfile(data): Observable<any> {
-    return this.http.post<any>('http://127.0.0.1:5000/updateUserProfile', data, { withCredentials: true });
+    return this.http.post<any>('https://peerconnecthubflask.onrender.com/updateUserProfile', data, { withCredentials: true });
   }
 }
